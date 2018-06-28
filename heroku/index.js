2001b100a843eafdf0577f7159c2ab9b8a71664b
Javier Escalona <javier@localhost.localdomain>
2018-06-28 16:51:47 -0300

    Se agrego conexiones a BD remota

--- heroku/index.js
+++ heroku/index.js
@@ -10,6 +10,10 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var xhub = require('express-x-hub');
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
@@ -22,8 +26,11 @@ var received_updates = [];
 
 app.get('/', function(req, res) {
   console.log(req);
-  res.send('<pre>JSON.stringify:' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send('<pre>Test</pre>');
 });
+
+
 
 app.get(['/facebook', '/instagram'], function(req, res) {   
   if (
@@ -35,6 +42,32 @@ app.get(['/facebook', '/instagram'], function(req, res) {
     res.sendStatus(400);
   }
 });
+
+app.get(['/bd'], function(req, res) {   
+var conString = process.env.ELEPHANTSQL_URL || "postgres://cgudwdkg:h7KwazdBN3u3KiI9_aMZabdf7V5ebrnQ@tantor.db.elephantsql.com:5432/cgudwdkg";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    res.send('<pre>could not connect to postgres: '+ err +'</pre>');
+    return console.error('could not connect to postgres', err);
+
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      res.send('<pre>error running query: '+ err +'</pre>');
+      return console.error('error running query', err);
+    }
+    res.send('<pre>error running query: '+ result.rows[0].theTime +'</pre>');
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
+
+});
+
+
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
